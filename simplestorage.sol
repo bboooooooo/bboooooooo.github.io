de43ce6e20ef044bd62826940d229f67558be928
bboooooooo <5minhyug@naver.com>
2018-07-25 10:39:32 +0900

    20180725(test)

--- simplestorage.sol
+++ simplestorage.sol
@@ -2,13 +2,13 @@ pragma solidity ^0.4.0;
 
 contract SimpleStorage
 {
-    string storedData;
+    bytes storedData;
 
-    function set(string x){
+    function set(bytes x){
         storedData = x;
     }
 
-    function get() constant returns (string) {
+    function get() constant returns (bytes) {
         return storedData;
     }
 }
